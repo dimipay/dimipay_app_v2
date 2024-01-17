f87299aec4bdb3e9c5e5efa4def5993f3cb93d61
Inhwa Jang <superjang3357@naver.com>
2024-01-17 18:24:20 +0900

    .env 패키지 추가 loader에서 초기화

--- lib/app/core/utils/loader.dart
+++ lib/app/core/utils/loader.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppLoader {
   Future<void> load() async {
-    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    WidgetsFlutterBinding.ensureInitialized();
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    await dotenv.load(fileName: "env/.env");
   }
 }
