bcad23c6938fb3530a6adb5f78c85adb032c79a6
Inhwa Jang <superjang3357@naver.com>
2024-01-17 18:06:45 +0900

    getx 패키지 추가 및 loader, route관련 세팅

---
+++ lib/app/pages/test/page.dart
@@ -0,0 +1,32 @@
+import 'package:dimipay_app_v2/app/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TestPage extends StatelessWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  Widget linkToRoute(String route) {
+    return TextButton(
+      onPressed: () {
+        Get.toNamed(route);
+      },
+      child: Text(route),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Route"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          linkToRoute(Routes.HOME),
+        ],
+      ),
+    );
+  }
+}
