15610930f4b4d28b27805d457d2cb15ad65968f7
Inhwa Jang <superjang3357@naver.com>
2024-01-19 22:23:13 +0900

    add log dev alias

--- lib/app/services/user/service.dart
+++ lib/app/services/user/service.dart
@@ -1,4 +1,4 @@
-import 'dart:developer';
+import 'dart:developer' as dev;
 
 import 'package:dimipay_app_v2/app/services/user/model.dart';
 import 'package:dimipay_app_v2/app/services/user/repository.dart';
@@ -7,14 +7,13 @@ import 'package:get/get.dart';
 class UserService extends GetxController with StateMixin<User?> {
   final UserRepository repository;
 
-  UserService({UserRepository? repository})
-      : repository = repository ?? UserRepository();
+  UserService({UserRepository? repository}) : repository = repository ?? UserRepository();
 
   final Rx<User?> _user = Rx(null);
   User? get user => _user.value;
 
   Future fetchUser() async {
-    log("Fetching user info");
+    dev.log("Fetching user info");
     try {
       change(user, status: RxStatus.loading());
       _user.value = await repository.getUserInfo();
