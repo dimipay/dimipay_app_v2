3fa45341b0d8234378ece778458b7a04e5134efa
dhalsdyd <minddong4992@gmail.com>
2024-01-27 14:40:15 +0900

    Add Payment Route

--- lib/app/pages/payment/page.dart
+++ lib/app/pages/payment/page.dart
@@ -7,83 +7,85 @@ class PaymentPage extends GetView<PaymentPageController> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Flexible(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Form(
-                  key: controller.formKey,
-                  child: FocusScope(
-                    node: controller.formFocusScopeNode,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 32),
-                        TextFormField(
-                          enableInteractiveSelection: false,
-                          controller: controller.cardNumberFieldController,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            labelText: '카드 번호',
-                            hintText: '0000-0000-0000-0000',
-                          ),
-                          maxLength: 19,
-                        ),
-                        const SizedBox(height: 16),
-                        Row(
-                          children: [
-                            Flexible(
-                              child: TextFormField(
-                                enableInteractiveSelection: false,
-                                controller: controller.expiredDateFieldController,
-                                keyboardType: TextInputType.number,
-                                decoration: const InputDecoration(
-                                  labelText: '유효기간',
-                                  hintText: 'MM/YY',
-                                ),
-                                maxLength: 5,
-                              ),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: Form(
+                    key: controller.formKey,
+                    child: FocusScope(
+                      node: controller.formFocusScopeNode,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 32),
+                          TextFormField(
+                            enableInteractiveSelection: false,
+                            controller: controller.cardNumberFieldController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              labelText: '카드 번호',
+                              hintText: '0000-0000-0000-0000',
                             ),
-                            const SizedBox(width: 16),
-                            Flexible(
-                              child: TextFormField(
-                                enableInteractiveSelection: false,
-                                controller: controller.ownerPersonalNumFieldController,
-                                keyboardType: TextInputType.number,
-                                decoration: const InputDecoration(
-                                  labelText: '생년월일 / 사업자번호',
-                                  hintText: '6 / 10자리',
-                                ),
-                                maxLength: 10,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 16),
-                        TextFormField(
-                          enableInteractiveSelection: false,
-                          controller: controller.passwordFieldController,
-                          keyboardType: TextInputType.number,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            labelText: '카드 비밀번호',
-                            hintText: '앞 2자리',
+                            maxLength: 19,
                           ),
-                          maxLength: 2,
-                        ),
-                        const SizedBox(height: 24),
-                      ],
+                          const SizedBox(height: 16),
+                          Row(
+                            children: [
+                              Flexible(
+                                child: TextFormField(
+                                  enableInteractiveSelection: false,
+                                  controller: controller.expiredDateFieldController,
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                    labelText: '유효기간',
+                                    hintText: 'MM/YY',
+                                  ),
+                                  maxLength: 5,
+                                ),
+                              ),
+                              const SizedBox(width: 16),
+                              Flexible(
+                                child: TextFormField(
+                                  enableInteractiveSelection: false,
+                                  controller: controller.ownerPersonalNumFieldController,
+                                  keyboardType: TextInputType.number,
+                                  decoration: const InputDecoration(
+                                    labelText: '생년월일 / 사업자번호',
+                                    hintText: '6 / 10자리',
+                                  ),
+                                  maxLength: 10,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            enableInteractiveSelection: false,
+                            controller: controller.passwordFieldController,
+                            keyboardType: TextInputType.number,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              labelText: '카드 비밀번호',
+                              hintText: '앞 2자리',
+                            ),
+                            maxLength: 2,
+                          ),
+                          const SizedBox(height: 24),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
