491d63afc346d14941025c191e9c3306df09f7a4
Inhwa Jang <superjang3357@naver.com>
2024-02-12 23:26:54 +0900

    splash screen loader

--- lib/app/core/utils/loader.dart
+++ lib/app/core/utils/loader.dart
@@ -3,25 +3,26 @@ import 'dart:io';
 import 'package:dimipay_app_v2/app/provider/api.dart';
 import 'package:dimipay_app_v2/app/provider/api_interface.dart';
 import 'package:dimipay_app_v2/app/services/auth/service.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 class AppLoader {
   Future<void> load() async {
-    WidgetsFlutterBinding.ensureInitialized();
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     await dotenv.load(fileName: "env/.env", isOptional: true);
     Get.lazyPut<ApiProvider>(() => DevApiProvider());
 
     await Get.putAsync(() => AuthService().init());
-    if (!kIsWeb) { // 웹에서는 Platform.isAndroid 사용 불가, 부득이하게 이중 If문 사용
-      if (Platform.isAndroid) {
-        FlutterDisplayMode.setHighRefreshRate();
-      }
+    // 웹에서는 Platform.isAndroid 사용 불가, 부득이하게 이중 If문 사용
+    if (Platform.isAndroid) {
+      FlutterDisplayMode.setHighRefreshRate();
     }
+    FlutterNativeSplash.remove();
   }
 }
