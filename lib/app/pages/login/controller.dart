5c28b376b2ef65313a01ee5c13e70e1657d65f6a
Seungpyo Suh <sspyorea@gmail.com>
2024-04-02 20:26:29 +0900

    fix: login error

--- lib/app/pages/login/controller.dart
+++ lib/app/pages/login/controller.dart
@@ -2,6 +2,7 @@ import 'package:dimipay_app_v2/app/core/utils/errors.dart';
 import 'package:dimipay_app_v2/app/routes/routes.dart';
 import 'package:dimipay_app_v2/app/services/auth/service.dart';
 import 'package:dimipay_app_v2/app/widgets/snackbar.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class LoginPageController extends GetxController {
@@ -13,14 +14,19 @@ class LoginPageController extends GetxController {
   Future loginWithGoogle() async {
     try {
       _isGoogleLoginInProgress.value = true;
-      if (authService.isFirstVisit) {
-        Get.offNamed(Routes.ONBOARDING, arguments: {'redirect': redirect});
-      } else {
-        final String nextRoute = redirect ?? Routes.HOME;
-        Get.offNamed(nextRoute);
+      await authService.loginWithGoogle();
+      if (authService.isGoogleLoginSuccess) {
+        if (authService.isFirstVisit) {
+          Get.offNamed(Routes.ONBOARDING, arguments: {'redirect': redirect});
+        } else {
+          final String nextRoute = redirect ?? Routes.HOME;
+          Get.offNamed(nextRoute);
+        }
       }
     } on NotDimigoMailExceptoin {
       DPErrorSnackBar().open('@dimigo.hs.kr로만 가입할 수 있어요!');
+    } on DioException catch (e) {
+      DPErrorSnackBar().open(e.response?.data['message'] ?? '');
     } finally {
       _isGoogleLoginInProgress.value = false;
     }
