c3c17471e70f787d29a05a0bc651c9fccf6341eb
Inhwa Jang <superjang3357@naver.com>
2024-06-04 16:57:01 +0900

    bigger dpappbar backbutton tap boundary

--- lib/app/widgets/appbar.dart
+++ lib/app/widgets/appbar.dart
@@ -1,4 +1,3 @@
-import 'package:dimipay_app_v2/app/widgets/button.dart';
 import 'package:dimipay_design_kit/dimipay_design_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -18,38 +17,46 @@ class DPAppbar extends StatelessWidget {
       bottom: false,
       child: Container(
         width: double.infinity,
-        padding: const EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             leading != null
                 ? leading!
-                : DPButton(
+                : GestureDetector(
                     onTap: () => Get.back(),
-                    radius: BorderRadius.circular(20),
-                    isTapEffectEnabled: false,
-                    child: Icon(Icons.arrow_back_ios_rounded, size: 20, color: colorTheme.grayscale500),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                      color: colorTheme.grayscale100,
+                      child: Icon(Icons.arrow_back_ios_rounded, size: 20, color: colorTheme.grayscale500),
+                    ),
                   ),
-            if (header != null)
-              Column(
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 16),
+              child: Column(
                 children: [
-                  const SizedBox(height: 16),
-                  Text(
-                    header!,
-                    style: textTheme.header1.copyWith(color: colorTheme.grayscale1000),
-                  ),
+                  if (header != null)
+                    Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        Text(
+                          header!,
+                          style: textTheme.header1.copyWith(color: colorTheme.grayscale1000),
+                        ),
+                      ],
+                    ),
+                  if (paragraph != null)
+                    Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        Text(
+                          paragraph!,
+                          style: textTheme.paragraph1.copyWith(color: colorTheme.grayscale700),
+                        ),
+                      ],
+                    ),
                 ],
               ),
-            if (paragraph != null)
-              Column(
-                children: [
-                  const SizedBox(height: 16),
-                  Text(
-                    paragraph!,
-                    style: textTheme.paragraph1.copyWith(color: colorTheme.grayscale700),
-                  ),
-                ],
-              ),
+            ),
           ],
         ),
       ),
