f000e27d67a137405b4800d77b0f1ea6cbdf41dc
Inhwa Jang <superjang3357@naver.com>
2024-08-08 14:08:36 +0900

    remove kreleasemode in loader

--- lib/app/core/utils/loader.dart
+++ lib/app/core/utils/loader.dart
@@ -3,7 +3,6 @@ import 'package:dimipay_app_v2/app/provider/api.dart';
 import 'package:dimipay_app_v2/app/provider/api_interface.dart';
 import 'package:dimipay_app_v2/app/services/auth/service.dart';
 import 'package:dimipay_app_v2/app/services/theme/service.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
@@ -19,12 +18,8 @@ class AppLoader {
     FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    
-    if (kReleaseMode) {
-      Get.lazyPut<SecureApiProvider>(() => ProdSecureApiProvider());
-    } else {
-      Get.lazyPut<SecureApiProvider>(() => DevSecureApiProvider());
-    }
+
+    Get.lazyPut<SecureApiProvider>(() => DevSecureApiProvider());
 
     await dotenv.load(fileName: "env/.env", isOptional: true);
     await Hive.initFlutter();
