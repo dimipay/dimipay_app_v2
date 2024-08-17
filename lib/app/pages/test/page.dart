b9c4f3c639c8748f7ad9613ab5c0e8b500b1d8b7
Seungpyo Suh <me@sspzoa.io>
2024-08-17 15:51:11 +0900

    refactor: refactor routes

--- lib/app/pages/test/page.dart
+++ lib/app/pages/test/page.dart
@@ -30,36 +30,51 @@ class TestPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Route"),
+        title: const Text("Route Test Page"),
         centerTitle: true,
       ),
       body: ListView(
         physics: const BouncingScrollPhysics(),
         children: [
+          // Root
           linkToRoute(Routes.HOME),
+
+          // Authentication
           linkToRoute(Routes.LOGIN),
+          linkToRoute(Routes.PW_LOGIN),
           linkToRoute(Routes.ONBOARDING),
-          linkToRoute(Routes.MANUAL),
           linkToRoute(Routes.PIN),
           linkToRouteWithArgs(
               Routes.PIN, "/edit_pin", {"pinPageType": PinPageType.editPin}),
+          linkToRoute(Routes.MANUAL),
+
+          // Main features
           linkToRoute(Routes.PAYMENT),
           linkToRoute(Routes.REGISTER_CARD),
           linkToRoute(Routes.EDIT_CARD),
-          linkToRoute(Routes.INFO),
           linkToRoute(Routes.TRANSACTION),
           linkToRoute(Routes.TRANSACTION_DETAIL),
+
+          // Settings and Info
+          linkToRoute(Routes.INFO),
           linkToRoute(Routes.THEME_SELECT),
           linkToRoute(Routes.FACESIGN),
           linkToRoute(Routes.VERSION),
           linkToRoute(Routes.LICENSE),
           linkToRoute(Routes.TERMS_OF_SERVICE),
           linkToRoute(Routes.PRIVACY_POLICY),
+
+          // Admin
           linkToRoute(Routes.ADMIN),
           linkToRoute(Routes.GENERATE_COUPON),
           linkToRoute(Routes.COUPON),
           linkToRoute(Routes.GENERATE_PASSCODE),
           linkToRoute(Routes.PASSCODE),
+
+          // Miscellaneous
+          linkToRoute(Routes.TEST),
+
+          // Utility
           TextButton(
             onPressed: () {
               Get.find<AuthService>().logout();
