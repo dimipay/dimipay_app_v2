b1dbf709aed7c206969445a56a3889f24f4cd286
Inhwa Jang <superjang3357@naver.com>
2024-11-25 21:09:12 +0900

    pass t to encryptPrivatePayload

--- test/local_pay_test.dart
+++ test/local_pay_test.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:base45/base45.dart';
 import 'package:dimipay_app_v2/app/services/pay/local_pay/local_pay.dart';
 
 Future<int> main() async {
@@ -5,16 +8,16 @@ Future<int> main() async {
     userIdentifier: '320fae03-9d72-4c19-816d-2c2d1d5b7ca2',
     deviceIdentifier: '265bed1a-9b4a-47fc-8765-b421d67a1458',
     authToken: 'adca79ec-7934-433c-acc0-a23088f39f58',
-    rk: 'c0093def64d3b1880da182de861cec39',
+    rk: Uint8List.fromList([0xc0, 0x09, 0x3d, 0xef, 0x64, 0xd3, 0xb1, 0x88, 0x0d, 0xa1, 0x82, 0xde, 0x86, 0x1c, 0xec, 0x39]),
   );
 
-  String res = await localPay.generateLocalPayToken(
+  Uint8List res = await localPay.generateLocalPayToken(
     paymentMethodIdentifier: 1,
-    paymentMethodCreatedAt: 1705896544745,
+    t0: 1705896544745,
     t: 1721089757738,
-    nonce: '01934f0337777ac38dcb8066c646b7fb',
+    nonce: '01934f03-3777-7ac3-8dcb-8066c646b7fb',
   );
 
-  print(res);
+  print(Base45.encode(res));
   return 0;
 }
