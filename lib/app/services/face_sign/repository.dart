a5dfdcbc864f3940e6c8f7ea03557dcebca14614
Inhwa Jang <superjang3357@naver.com>
2024-12-02 17:12:01 +0900

    feat : facesign cache

--- lib/app/services/face_sign/repository.dart
+++ lib/app/services/face_sign/repository.dart
@@ -1,9 +1,12 @@
 import 'package:dimipay_app_v2/app/core/utils/errors.dart';
 import 'package:dimipay_app_v2/app/provider/api_provider.dart';
+import 'package:dimipay_app_v2/app/provider/middlewares/from_cache.dart';
 import 'package:dimipay_app_v2/app/provider/middlewares/jwt.dart';
+import 'package:dimipay_app_v2/app/provider/middlewares/save_cache.dart';
 import 'package:dimipay_app_v2/app/provider/model/request.dart';
 import 'package:dimipay_app_v2/app/provider/model/response.dart';
 import 'package:dimipay_app_v2/app/provider/providers/dio.dart';
+import 'package:dimipay_app_v2/app/services/cache/service.dart';
 import 'package:dio/dio.dart';
 import 'package:get/instance_manager.dart';
 import 'package:image_picker/image_picker.dart';
@@ -15,10 +18,35 @@ class FaceSignRepository {
 
   FaceSignRepository({ApiProvider? api}) : api = api ?? Get.find<ApiProvider>();
 
-  Future<bool> checkIfFaceSignRegistered() async {
+  Future<bool> getFaceSignState() async {
     String url = '/face-sign';
-    DPHttpResponse response = await api.get(DPHttpRequest(url), [JWT()]);
+    DPHttpResponse response = await api.get(DPHttpRequest(url), [JWT(), SaveCache()]);
     return response.data['registered'];
+  }
+
+  Future<bool> getFaceSignStateFromCache() async {
+    String url = '/face-sign';
+    DPHttpResponse response = await api.get(DPHttpRequest(url), [FromCache()]);
+    return response.data['registered'];
+  }
+
+  Future<void> saveCurrentStateToCache(bool isRegisterd) async {
+    String url = '/face-sign';
+    HttpCacheService cacheService = Get.find<HttpCacheService>();
+
+    Map<String, dynamic> data = {
+      'registered': isRegisterd,
+    };
+
+    await cacheService.save(
+        DPHttpRequest(url, method: 'GET'),
+        DPHttpResponse(
+          requredId: '',
+          code: 'OK',
+          statusCode: 200,
+          timeStamp: DateTime.now().toString(),
+          data: data,
+        ));
   }
 
   Future<void> registerFaceSign(XFile file) async {
