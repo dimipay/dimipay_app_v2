b846b31f3edb78ad3b567935f522a319e2a6fb4e
Yeonjun Lee <yeonfish6040@gmail.com>
2025-02-19 20:43:42 +0900

    chore: skip face sign registration

--- lib/app/pages/onboarding/page.dart
+++ lib/app/pages/onboarding/page.dart
@@ -26,7 +26,7 @@ class OnboardingPage extends GetView<OnboardingPageController> {
                 TermsAgreementPage(controller: controller),
                 ManualPage(controller: controller),
                 SuggestCardRegistratoinPage(controller: controller),
-                SuggestFaceSignRegistrationPage(controller: controller),
+                // SuggestFaceSignRegistrationPage(controller: controller),
               ],
             ),
           ),
